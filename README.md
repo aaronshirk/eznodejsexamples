9092cf83a2f10eea41f0f0e3562c078bdc436ec6
Aaron Shirk <ashirk@underarmour.com>
2014-03-21 07:25:11 -0400

    initial commit with example1 source code - hello world

---
+++ README.md
@@ -0,0 +1,7 @@
+# Ez Node.js Examples
+
+Sample code for the [eznodejsexmaples blog](http://eznodejsexamples.com).
+
+## Organization
+
+The subfolders in this project contain the source code for each example provided on the blog.
---
+++ example1-helloworld/helloworld3.js
@@ -0,0 +1,9 @@
+var http = require('http');
+
+http.createServer(function(req, res) {
+	res.writeHead(200, {'Content-Type' : 'text/plain'});
+	res.write('hello world');
+	res.end();
+}).listen(8081,function() {
+	console.log('listening on port 8081');
+});
